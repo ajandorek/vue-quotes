f1d0cba2b0dc491f89af8be81b764b3a083426c5
Alec Jandorek <ajandorek@gmail.com>
2018-05-02 10:56:38 -0500

    Created quote component and grid and started implementation of new form

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
     <div class="container">
-    
+        <appQuoteGrid :quotes="quotes">
+        </appQuoteGrid>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+import QuoteGrid from './components/QuoteGrid.vue';
+export default {
+  data: function() {
+    return {
+      quotes: ['Just a quote to see something', 'quote 2', 'quote 3'],
+      maxQuotes: 10
+    };
+  },
+  components: {
+    appQuoteGrid: QuoteGrid
+  }
+};
 </script>
 
 <style>
+
 </style>
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <appQuote v-for="(quote, i) in quotes" :key="i">
+      {{quote}}
+    </appQuote>
+  </div>  
+</template>
+<script>
+import Quote from './Quote.vue';
+
+export default {
+  props: ['quotes'],
+  components: {
+    appQuote: Quote
+  }
+};
+</script>
+<style>
+
+</style>
+
+
